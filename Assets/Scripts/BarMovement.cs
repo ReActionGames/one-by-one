666d7cee8a8dd8a84f5c21bf7067568adc4ed55a
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-07-20 18:59:33 -0400

    Added bars

---
+++ Assets/Scripts/BarMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarMovement : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform white;
+    [SerializeField] private Transform start, end;
+
+    private float direction = 1;
+    private float positionPercentage = 0;
+    private bool isMoving = true;
+
+    private void Update()
+    {
+        if (isMoving == false)
+            return;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        positionPercentage += Time.deltaTime * direction;
+
+        white.position = Vector3.Lerp(start.position, end.position, positionPercentage);
+
+        if (positionPercentage >= 1 && direction == 1)
+        {
+            direction = -1;
+        }
+        else if (positionPercentage <= 0 && direction == -1)
+        {
+            direction = 1;
+        }
+    }
+
+    public void StopMoving()
+    {
+        isMoving = false;
+    }
+}
---
+++ Assets/Scripts/BarStopper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarStopper : MonoBehaviour {
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StopBar();
+        }
+    }
+
+    private void StopBar()
+    {
+        GetComponent<BarMovement>().StopMoving();
+    }
+}
