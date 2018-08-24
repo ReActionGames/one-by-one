cf751edf208a6e8338492f4ceaff41ea41f07298
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-08-24 09:25:04 -0400

    Imported DoozyUI package and added endGame screen

---
+++ Assets/Scripts/UI/UIEndGame.cs
@@ -0,0 +1,68 @@
+﻿using DoozyUI;
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIEndGame : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private float showScreenDelay;
+
+    private UIElement endGameElement;
+
+    private void Awake()
+    {
+        endGameElement = GetComponent<UIElement>();
+    }
+
+    private void OnEnable()
+    {
+        var player = FindObjectOfType<Player>();
+        if (player)
+        {
+            player.OnEdgeColliderHit += HandleEdgeColliderHit;
+        }
+    }
+
+    private void OnDisable()
+    {
+        var player = FindObjectOfType<Player>();
+        if (player)
+        {
+            player.OnEdgeColliderHit -= HandleEdgeColliderHit;
+        }
+    }
+
+    private void HandleEdgeColliderHit()
+    {
+        PrepareEndGameScreen();
+        ShowEndGameScreenAfterDelay(showScreenDelay);
+    }
+
+    private void ShowEndGameScreenAfterDelay(float delay)
+    {
+        var sequence = DOTween.Sequence();
+        sequence.PrependInterval(delay)
+            .OnComplete(ShowEndGameScreen);
+        sequence.Play();
+    }
+
+    private void ShowEndGameScreen()
+    {
+        endGameElement.Show(false);
+    }
+
+    private void PrepareEndGameScreen()
+    {
+        scoreText.text = FindObjectOfType<ScoreKeeper>().Score.ToString();
+    }
+
+    public void OnRestartClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
