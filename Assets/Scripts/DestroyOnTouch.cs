7c29ad55b7b80009de581f1d11dd7a5134cc05fa
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-09-07 14:37:38 -0400

    Destroy player pieces after the leave the screen to improve performance

---
+++ Assets/Scripts/DestroyOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DestroyOnTouch : MonoBehaviour
+{
+    [SerializeField] private LayerMask collisionLayer;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == collisionLayer)
+            Destroy(collider.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.layer == collisionLayer)
+            Destroy(collision.gameObject);
+
+    }
+
+
+}
