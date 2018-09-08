fb561542a74bbf487e9b522123565454c65a8ad7
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-09-08 18:29:54 -0400

    Added player shadows

---
+++ Assets/Scripts/DampFollow.cs
@@ -0,0 +1,69 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DampFollow : MonoBehaviour {
+
+    //[SerializeField] private bool followParent = false;
+    [SerializeField/*, HideIf("followParent", false)*/] private Transform followTarget;
+    [SerializeField] private float dampning;
+    [SerializeField] private bool followBackwards = false;
+
+    private Vector3 targetLastPosition;
+    private bool targetHasMovedFlag;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        //if (followParent)
+        //{
+        //    transform.localPosition = Vector3.zero;
+        //    followTarget = transform.parent;
+        //}
+        //if (!followParent)
+            transform.position = followTarget.position;
+        targetHasMovedFlag = false;
+        targetLastPosition = followTarget.position;
+    }
+
+    private void Update()
+    {
+        //CheckIfTargetHasMoved();
+        //if (targetHasMovedFlag)
+        //{
+            UpdatePosition();
+        //    return;
+        //}
+        targetLastPosition = followTarget.position;
+
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 targetPosition = followTarget.position;
+
+        if (!followBackwards && HasTargetMovedBackwards())
+        {
+            transform.position = targetPosition;
+            return;
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, dampning);
+    }
+
+    private bool HasTargetMovedBackwards()
+    {
+        Vector3 direction = followTarget.position - targetLastPosition;
+        if (direction.y < 0)
+            return true;
+        return false;
+    }
+
+    private bool CheckIfTargetHasMoved()
+    {
+        targetHasMovedFlag = followTarget.position == targetLastPosition;
+        return targetHasMovedFlag;
+    }
+}
---
+++ Assets/Scripts/DisableOnPlayerDeath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class DisableOnPlayerDeath : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Player player = FindObjectOfType<Player>();
+        if (player)
+        {
+            player.OnEdgeColliderHit += Disable;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Player player = FindObjectOfType<Player>();
+        if (player)
+        {
+            player.OnEdgeColliderHit -= Disable;
+        }
+    }
+
+    private void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
