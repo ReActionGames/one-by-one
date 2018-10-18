86ce40ddc38ef1f7e0d08df4f1638686ce0fc806
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-10-18 18:32:22 -0400

    Added MonoBehaviourSingleton abstract class and GameManager

---
+++ Assets/AddIns/MonoBehaviourSingleton.cs
@@ -0,0 +1,42 @@
+﻿namespace GLITCH.DoctrinalMastery.Structure
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// A singleton base class for easier singleton implementation.
+    /// </summary>
+    /// <typeparam name="TSelfType">The type of the class inheriting this class.</typeparam>
+    public class MonoBehaviourSingleton<TSelfType> : MonoBehaviour where TSelfType : MonoBehaviour
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// The single instance of this class.
+        /// </summary>
+        private static TSelfType m_Instance = null;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// The single instance of this class.s
+        /// </summary>
+        public static TSelfType Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = (TSelfType)FindObjectOfType(typeof(TSelfType));
+                    if (m_Instance == null)
+                        m_Instance = (new GameObject(typeof(TSelfType).Name)).AddComponent<TSelfType>();
+                    DontDestroyOnLoad(m_Instance.gameObject);
+                }
+                return m_Instance;
+            }
+        }
+
+        #endregion Public Properties
+    }
+}
