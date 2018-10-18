6e5a5ce6844cd759660265e3898453f5b4674106
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-10-18 18:25:06 -0400

    Replaced placeholder sprites with single sprite

--- Assets/Scripts/HighScoreMarker.cs
+++ Assets/Scripts/HighScoreMarker.cs
@@ -6,14 +6,18 @@ public class HighScoreMarker : MonoBehaviour, IResetable
 {
     private const int numberOfBars = 10;
 
-    [SerializeField] private Transform bar, left, right, topOfScreen;
-    [SerializeField] private GameObject[] fragments;
+    [SerializeField] private Transform /*bar, left, right, */topOfScreen;
+    [SerializeField] private SpriteRenderer marker;
+    //[SerializeField] private GameObject[] fragments;
+    [SerializeField] private float fadeDuration;
 
     private ScoreKeeper scoreKeeper;
     private bool gotHighScoreThisRound = false;
+    private float normalAlphaValue;
 
     private void Awake()
     {
+        normalAlphaValue = marker.color.a;
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
@@ -41,11 +45,29 @@ public class HighScoreMarker : MonoBehaviour, IResetable
 
     private void CheckForHighScore(float duration, Ease easing)
     {
-        if (scoreKeeper.HighScore <= 0 || scoreKeeper.Score + numberOfBars <= scoreKeeper.HighScore || gotHighScoreThisRound)
+        Debug.Log($"[{Time.time}] CheckForHighScore() - first pass");
+        if (scoreKeeper.HighScore <= 0)
+        {
+            Debug.Log($"[{Time.time}] CheckForHighScore() - second pass");
             return;
+        }
+
+        if (scoreKeeper.Score + numberOfBars <= scoreKeeper.HighScore)
+        {
+            Debug.Log($"[{Time.time}] CheckForHighScore() - third pass");
+            return;
+        }
+
+        if (gotHighScoreThisRound)
+        {
+            Debug.Log($"[{Time.time}] CheckForHighScore() - forth pass");
+            return;
+        }
+
+        Debug.Log($"[{Time.time}] CheckForHighScore() - fifth pass");
 
         int scoreDelta = scoreKeeper.HighScore - scoreKeeper.Score;
-        transform.position = new Vector3(0, scoreDelta * 2);
+        transform.position = new Vector3(0, (scoreDelta * 2) + 2);
         Vector3 endPosition = transform.position;
         transform.position += topOfScreen.position;
         SetMarkerActive(true);
@@ -62,29 +84,37 @@ public class HighScoreMarker : MonoBehaviour, IResetable
     [Button]
     private void ResizeMarkerToFitCamera()
     {
+        Debug.Log($"[{Time.time}] ResizeMarkerToFitCamera()");
         float height = Camera.main.orthographicSize * 2.0f;
         float width = height * Camera.main.aspect;
 
-        bar.localScale = new Vector3(width, bar.localScale.y);
+        marker.size = new Vector2(width, marker.size.y);
 
-        float edgePosition = (width / 2) - right.GetComponent<SpriteRenderer>().bounds.extents.x;
+        //bar.localScale = new Vector3(width, bar.localScale.y);
 
-        right.localPosition = new Vector3(edgePosition, right.localPosition.y);
+        //float edgePosition = (width / 2) - right.GetComponent<SpriteRenderer>().bounds.extents.x;
 
-        left.localPosition = new Vector3(-edgePosition, left
-.localPosition.y);
+        //right.localPosition = new Vector3(edgePosition, right.localPosition.y);
+
+        //left.localPosition = new Vector3(-edgePosition, left
+//.localPosition.y);
     }
 
     private void SetMarkerActive(bool active)
     {
-        bar.gameObject.SetActive(active);
-        right.gameObject.SetActive(active);
-        left.gameObject.SetActive(active);
+        Debug.Log($"[{Time.time}] SetMarkerActive({active})");
+        //bar.gameObject.SetActive(active);
+        //right.gameObject.SetActive(active);
+        //left.gameObject.SetActive(active);
+        marker.gameObject.SetActive(active);
+        marker.DOFade(normalAlphaValue, 0);
     }
 
     private void OnNewHighScore()
     {
-        SetMarkerActive(false);
+        Debug.Log($"[{Time.time}] OnNewHighScore()");
+        //SetMarkerActive(false);
+        marker.DOFade(0, fadeDuration);
         gotHighScoreThisRound = true;
 
         //foreach (var frag in fragments)
@@ -97,6 +127,7 @@ public class HighScoreMarker : MonoBehaviour, IResetable
 
     public void ResetObject()
     {
+        Debug.Log($"[{Time.time}] ResetObject()");
         SetMarkerActive(false);
         gotHighScoreThisRound = false;
     }
