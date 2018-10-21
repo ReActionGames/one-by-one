1f6847f5d7fc87f2736334c8f39b0e5e402f7fb7
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-10-21 17:08:47 -0400

    Added EventManager

---
+++ Assets/Scripts/EventNames.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReActionGames.Events
+{
+    public static class EventNames
+    {
+        public const string PowerUpCollected = "powerup-collected";
+    }
+}
