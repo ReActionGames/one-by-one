bb4ae047736449b0ed880ad9ef85edd7cabc160f
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-10-22 18:45:52 -0400

    Started coding shield logic

---
+++ Assets/Scripts/Player/PlayerShield.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShield : MonoBehaviour {
+
+    [SerializeField] private ShieldProperties properties;
+    [SerializeField] private GameObject shieldIndicator;
+    [SerializeField] private SpriteRenderer movingSprite;
+
+    private Tween spriteTween;
+
+	public void Activate()
+    {
+        shieldIndicator.SetActive(true);
+        if (spriteTween == null)
+            InitiateSpriteTween();
+    }
+
+    private void InitiateSpriteTween()
+    {
+        spriteTween = movingSprite.transform.DOScale(1, properties.Duration)
+            .From()
+            .SetEase(properties.Ease)
+            .SetLoops(-1);
+    }
+
+    public void Deactivate()
+    {
+        shieldIndicator.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Player/ShieldProperties.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Shield Properties", menuName = "Scriptable Objects/Shield Properties")]
+public class ShieldProperties : ScriptableObject
+{
+    [SerializeField] private float duration;
+    [SerializeField] private Ease ease;
+
+    public float Duration => duration;
+    public Ease Ease => ease;
+}
