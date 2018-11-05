df4efe5f8b96702419cde21d3fde9454c30d6a75
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-11-05 18:22:26 -0500

    Added shield powerup sound effect

--- Assets/Scripts/PowerUps/ShieldPowerUp.cs
+++ Assets/Scripts/PowerUps/ShieldPowerUp.cs
@@ -3,10 +3,18 @@ using ReActionGames;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DoozyUI;
 
 public class ShieldPowerUp : PowerUp {
 
     [SerializeField] private SpriteRenderer sprite;
+
+    private SoundEffect soundEffect;
+
+    private void Awake()
+    {
+        soundEffect = GetComponent<SoundEffect>();
+    }
 
     public void SetUp()
     {
@@ -16,7 +24,8 @@ public class ShieldPowerUp : PowerUp {
 	public void Collect()
     {
         sprite.gameObject.SetActive(false);
-        EventManager.TriggerEvent(EventNames.PowerUpCollected, new Message(this));
+        soundEffect.PlaySoundEffect();
+        EventManager.TriggerEvent(EventNames.PowerUpCollected, new ReActionGames.Events.Message(this));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
