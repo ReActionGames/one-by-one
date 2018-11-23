154fa4df1ffc45130be0c62119dd79d315862879
GlitchBoss <12978318+GlitchBoss@users.noreply.github.com>
2018-11-23 07:36:07 -0500

    Added bar pool mover

---
+++ Assets/Continuous Mode/Scripts/BarPoolMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Continuous
+{
+    public class BarPoolMover
+    {
+        private Transform barParent;
+        private Transform currentBar;
+        private float speed = 1;
+
+        public BarPoolMover(Transform barParent)
+        {
+            this.barParent = barParent;
+        }
+
+        public void StartMoving()
+        {
+
+        }
+    }
+}
