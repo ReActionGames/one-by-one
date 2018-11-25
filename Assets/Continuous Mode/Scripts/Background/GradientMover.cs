59992e39181a47eb7d30de294a2243b2014ea0a0
Josh H <hendershotjoshr@gmail.com>
2018-11-24 21:25:23 -0500

    Added Gradient mover

---
+++ Assets/Continuous Mode/Scripts/Background/GradientMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Continuous
+{
+    public class GradientMover : MonoBehaviour, IBackgroundElementMover
+    {
+
+
+        public void StartMoving(float speed)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopMoving()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTimeScale(float time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
