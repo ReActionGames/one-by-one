e7807a1acf2f194b3bd843341b4a654e42233193
Josh H <hendershotjoshr@gmail.com>
2018-12-24 08:04:00 -0500

    Added Powerup Scripts

---
+++ Assets/Continuous Mode/Scripts/Powerups/PowerupComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Continuous
+{
+    public abstract class PowerupComponent : MonoBehaviour
+    {
+        public enum State
+        {
+            Active,
+            Inactive
+        }
+    }
+}
---
+++ Assets/Continuous Mode/Scripts/Powerups/ShieldPowerupComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Continuous
+{
+    public class ShieldPowerupComponent : PowerupComponent
+    {
+    }
+}
