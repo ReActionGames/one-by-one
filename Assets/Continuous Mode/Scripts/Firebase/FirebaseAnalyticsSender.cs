d186c45991a216b50a858f8826b8e7a930625052
Josh H <hendershotjoshr@gmail.com>
2019-01-28 09:45:29 -0500

    Added firebase analytics sender script

---
+++ Assets/Continuous Mode/Scripts/Firebase/FirebaseAnalyticsSender.cs
@@ -0,0 +1,25 @@
+﻿using Firebase.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Continuous
+{
+    public class FirebaseAnalyticsSender : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GameManager.GameEnd += SendOnGameEndData;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.GameEnd -= SendOnGameEndData;
+        }
+
+        private void SendOnGameEndData()
+        {            
+            //FirebaseAnalytics.LogEvent()
+        }
+    }
+}
