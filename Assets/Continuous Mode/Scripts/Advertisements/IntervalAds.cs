c8ced65141d4b3f6fb30d4998aafedc19c29812e
Josh H <hendershotjoshr@gmail.com>
2019-02-03 14:16:02 -0500

    Added no ads button functionality

--- Assets/Continuous Mode/Scripts/Advertisements/IntervalAds.cs
+++ Assets/Continuous Mode/Scripts/Advertisements/IntervalAds.cs
@@ -1,26 +1,36 @@
 ﻿using Sirenix.OdinInspector;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 namespace Continuous
 {
     public class IntervalAds : MonoBehaviour
     {
         private const string GamesPerAdKey = "games-per-ad";
-
+        private string NoAdsKey = "no-ads";
+        private const string NoAds_IAP_ID = "com.reactiongames.onebyone.no_ads";
 
         [SerializeField] private int defaultGamesPerAd = 3;
         [SerializeField] private float delay = 0.5f;
-
         [ReadOnly]
-        private int gamesUntilNextAd;
+        [SerializeField] private int gamesUntilNextAd;
 
+        private UINoAdsButton[] noAdsButtons;
         private int gamesPerAd;
+
+        private bool noAds
+        {
+            get => PlayerPrefs.GetInt(NoAdsKey, 0) == 1;
+            set => PlayerPrefs.SetInt(NoAdsKey, value == true ? 1 : 0);
+        }
 
         private void Awake()
         {
             LoadRemoteSettings();
             gamesUntilNextAd = gamesPerAd;
+            noAdsButtons = FindObjectsOfType<UINoAdsButton>();
+            UpdateNoAdsButtonActivation(noAds);
         }
 
         private void OnEnable()
@@ -44,6 +54,14 @@ namespace Continuous
             gamesPerAd = RemoteSettings.GetInt(GamesPerAdKey, defaultGamesPerAd);
         }
 
+        private void UpdateNoAdsButtonActivation(bool noAds)
+        {
+            foreach (UINoAdsButton button in noAdsButtons)
+            {
+                button.UpdateActivation(noAds);
+            }
+        }
+
         private void OnGameEnd()
         {
             gamesUntilNextAd--;
@@ -56,11 +74,19 @@ namespace Continuous
 
         private IEnumerator ShowAd()
         {
-            if (AdvertisementUtility.ShowAds)
+            if (noAds == false && AdvertisementUtility.ShowAds)
             {
                 yield return new WaitForSeconds(delay);
                 AdvertisementUtility.ShowVideoAd();
             }
+        }
+
+        public void NoAdsPurchaseComplete(Product product)
+        {
+            if (product.definition.id.Equals(NoAds_IAP_ID) == false) return;
+
+            noAds = true;
+            UpdateNoAdsButtonActivation(noAds);
         }
     }
 }
---
+++ Assets/Continuous Mode/Scripts/UI/UINoAdsButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+namespace Continuous
+{
+    public class UINoAdsButton : MonoBehaviour
+    {
+        public void NoAdsPurchaseComplete(Product product)
+        {
+            FindObjectOfType<IntervalAds>().NoAdsPurchaseComplete(product);
+        }
+
+        public void UpdateActivation(bool noAds)
+        {
+            StartCoroutine(SetActiveAfterDelay(!noAds));
+        }
+
+        private IEnumerator SetActiveAfterDelay(bool active)
+        {
+            yield return null;
+            gameObject.SetActive(active);
+        }
+    }
+}
