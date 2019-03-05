a75017992cbf230ece65b9e7f700cfe63c906d76
Josh H <hendershotjoshr@gmail.com>
2019-03-05 07:39:58 -0500

    Moved UI to UI scene

---
+++ Assets/Continuous Mode/Scripts/UI/UIButtonUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Continuous
+{
+    public class UIButtonUtility : MonoBehaviour
+    {
+        public void RestartGame()
+        {
+            GameManager.RestartGame();
+        }
+
+        public void StartGame()
+        {
+            GameManager.StartGame();
+        }
+    }
+}
---
+++ Assets/Continuous Mode/Scripts/UI/UISceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Continuous
+{
+    public class UISceneLoader : MonoBehaviour
+    {
+        [SerializeField] private string uiSceneName;
+
+        private void Start()
+        {
+            SceneManager.LoadScene(uiSceneName, LoadSceneMode.Additive);
+        }
+    }
+}
