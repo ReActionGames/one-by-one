0cf41214d4cf329afc1c3a6ec74ecfbb9202d247
Josh H <hendershotjoshr@gmail.com>
2019-03-15 07:23:15 -0400

    Fixed Double bar pickup positioning

--- Assets/Continuous Mode/Scripts/Bar/Double/DoubleBarPickupPositioner.cs
+++ Assets/Continuous Mode/Scripts/Bar/Double/DoubleBarPickupPositioner.cs
@@ -6,12 +6,18 @@ namespace Continuous
     {
         public void PositionPickup(Transform powerup, float size, float padding)
         {
-            // TODO: Position double bar pickup
-            float maxDistanceFromCenter = (size / 2) - padding;
+            float centerOffset = ((19 / 92f) * size) + (957 / 460f); // Predetermined formula (y = 19/92 * x + 957/460)
+            if (RandomExtensions.RandomBoolean())
+            {
+                centerOffset = -centerOffset;
+            }
 
-            float xPos = Random.Range(-maxDistanceFromCenter, maxDistanceFromCenter);
+            float adjustedSize = ((21 / 46f) * size) + (84 / 115f); // Predetermined formula (y = 21/46 * x + 84/115)
+            float maxDistanceFromCenter = (adjustedSize / 2) - padding;
 
-            powerup.localPosition = Vector3.zero.With(x: xPos);
+            float distanceFromCenter = Random.Range(-maxDistanceFromCenter, maxDistanceFromCenter);
+
+            powerup.localPosition = Vector3.zero.With(x: centerOffset + distanceFromCenter);
         }
     }
 }
